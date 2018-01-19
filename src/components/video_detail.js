545c2a138a9e4e0fcaa8f2fc2a3ad847c7fe64ce
Andres Saldarriaga <jandreus87@hotmail.com>
2018-01-19 09:49:09 -0500

    full

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const VideoDetail = ({video}) => {
-if (!video) {
-  return <div> LOADING...</div>
-}
-
+const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Loading...</div>;
+  }
 
   const videoId = video.id.videoId;
-  const url= `https://www.youtube.com/embed/ ${videoId}`;
+  const url = `https://www.youtube.com/embed/${videoId}`;
+
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
@@ -16,7 +16,6 @@ if (!video) {
       <div className="details">
         <div>{video.snippet.title}</div>
         <div>{video.snippet.description}</div>
-      
       </div>
     </div>
   );
